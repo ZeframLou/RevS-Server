21bc334b03a4c01f960d1f6400a7ed692d9bba99
theGreatLzbdd <the.great.lzbdd@gmail.com>
2013-10-30 12:33:07 +0800

    Copyright changes

--- Objective-C/AppDelegate.h
+++ Objective-C/AppDelegate.h
@@ -2,9 +2,26 @@
 //  AppDelegate.h
 //  RevS Server Test
 //
-//  Created by lzbdd on 13-10-4.
-//  Copyright (c) 2013年 lzbdd. All rights reserved.
-//
+//  Created by Zebang Liu on 13-10-4.
+//  Copyright (c) 2013年 Zebang Liu. All rights reserved.
+//  Contact: the.great.lzbdd@gmail.com
+/*
+ This file is part of RevS Server.
+ 
+ RevS is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ RevS is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public License
+ along with RevS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 #import <Cocoa/Cocoa.h>
 
